3fe735cc69e1f7cc376ad374f6f8134ea815aa68
Greentea231 <sharu290901@gmail.com>
2026-07-17 11:12:57 +0100

    Delete api directory

--- api/test-key.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  const hasKey = !!process.env.OPENAI_API_KEY;
-  res.json({
-    ok: true,
-    hasKey,
-    keyLength: process.env.OPENAI_API_KEY ? process.env.OPENAI_API_KEY.length : 0
-  });
-}
